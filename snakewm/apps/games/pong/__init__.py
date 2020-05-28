5947971b34f90b10a81bb49561bbd6a829ad7bf1
Josh Moore <jxm5210@g.rit.edu>
2020-05-28 09:59:23 -0400

    Fix documentation mistakes

--- snakewm/apps/games/pong/__init__.py
+++ snakewm/apps/games/pong/__init__.py
@@ -3,14 +3,15 @@ from .pongwindow import PongWindow
 def load(manager, params):
     """
     Create Pong game and add it to the UI manager.
-    params[0] should be the desired size, if other than default.
+    params[0] should be the desired position. A default
+    position will be used if a pos is not provided.
     """
-    default_size = (50, 50)
+    default_pos = (50, 50)
 
     if params is not None and len(params) > 0:
         # create PongWindow of user-specified size
         PongWindow(params[0], manager)
     else:
         # create PongWindow of default size
-        PongWindow(default_size, manager)
+        PongWindow(default_pos, manager)
     
