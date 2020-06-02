b714ced08db6682a75ba8acf5490f56d1384d6b5
HelgeCPH <ropf@itu.dk>
2020-06-02 22:45:20 +0200

    Add the oldschool commands list and run, with newschool edit
    Based on GarbageHamburger's pull request (https://github.com/joshiemoore/snakeware/pull/45), which suggests a command for starting the window manager, I built three commands that should add to the original home computer feeling. One can edit, list, and run a single program for which the file is 'hidden' from the user but stored as a file under

--- snakeware/overlay/usr/share/snakeware/startup.py
+++ snakeware/overlay/usr/share/snakeware/startup.py
@@ -1,9 +1,19 @@
+import subprocess
+from shutil import which
+from suplemon.main import App
+
+
+THE_CODE_FILE = "/tmp/code.py"
+THIS_PYTHON = "/usr/bin/python3"
+LESS_PRG = which("less")
+
+
 class Command:
     """Defines a command which is run when repr(self) is evaluated."""
 
     def run(self):
         raise NotImplementedError
-        
+
     def __repr__(self):
         """Execute the command."""
         return self.run()
@@ -13,7 +23,39 @@ class SnakeWMCommand(Command):
     def run(self):
         """Start SnakeWM."""
         from snakewm.wm import SnakeWM
+
         return SnakeWM().run()
 
 
+class EditCommand(Command):
+    """Start suplemon with this single file to edit."""
+
+    def __repr__(self):
+        app = App(filenames=[THE_CODE_FILE])
+        if app.init():
+            app.run()
+            return "I stored your code."
+
+
+class RunCommand:
+    """Run the only program that we have in THE_CODE_FILE."""
+
+    def __repr__(self):
+        _ = subprocess.run([THIS_PYTHON, THE_CODE_FILE])
+        return ""
+
+
+class ListCommand:
+    """Use less to show the contents of the only program that we have in 
+    THE_CODE_FILE."""
+
+    def __repr__(self):
+        _ = subprocess.run([LESS_PRG, THE_CODE_FILE])
+        return ""
+
+
 snakewm = SnakeWMCommand()
+
+edit = EditCommand()
+run = RunCommand()
+list = ListCommand()
