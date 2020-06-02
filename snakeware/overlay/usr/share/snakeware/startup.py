648eba1c1b1fbbf43a8f419d0272c2df88afe2df
GarbageHamburger <63806108+GarbageHamburger@users.noreply.github.com>
2020-06-02 17:51:38 +0000

    Abstract out command definition
    A new class called `Command' is created which commands inherit from.

--- snakeware/overlay/usr/share/snakeware/startup.py
+++ snakeware/overlay/usr/share/snakeware/startup.py
@@ -1,5 +1,17 @@
-class SnakeWMCommand:
+class Command:
+    """Defines a command which is run when repr(self) is evaluated."""
+
+    def run(self):
+        raise NotImplementedError
+        
     def __repr__(self):
+        """Execute the command."""
+        return self.run()
+
+
+class SnakeWMCommand(Command):
+    def run(self):
+        """Start SnakeWM."""
         from snakewm.wm import SnakeWM
         return SnakeWM().run()
 
