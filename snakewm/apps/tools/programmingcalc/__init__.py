5249a3d220f40e8adfd272b2ef9bc7a0622b5a7c
mochida <rahmanhakim2435@gmail.com>
2020-06-06 01:48:24 +0700

    Added programming calculator that could convert int to bin, and hex

---
+++ snakewm/apps/tools/programmingcalc/__init__.py
@@ -0,0 +1,14 @@
+from .programmingcalc import ProgrammingCalc
+
+
+def load(manager, params):
+    """
+    Create and launch a new instance of SnakeCalc.
+    """
+    # default position
+    pos = (100, 100)
+
+    if params is not None and len(params) > 0:
+        pos = params[0]
+
+    ProgrammingCalc(pos, manager)
