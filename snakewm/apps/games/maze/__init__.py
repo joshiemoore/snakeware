f4eb666aca0024357dea513c479b9b3c24f0de10
ChaosKid42 <christoph.scholz@gmail.com>
2020-06-06 19:52:12 +0200

    add a very simple maze game

---
+++ snakewm/apps/games/maze/__init__.py
@@ -0,0 +1,9 @@
+from .mazewindow import MazeWindow
+
+def load(manager, params):
+    pos = (100, 100)
+
+    if params is not None and len(params) > 0:
+        pos = params[0]
+
+    MazeWindow(pos, manager)
