5dbccffc9c20a970bb4de172d9aaf759b689e283
Alexandru Avadanii <alex@softwareelite.com>
2020-06-08 13:39:18 +0300

    build: Use FUSE to allow non-root builds
    Replace the manual building steps requiring root level access (e.g.
    mount, kpartx) with an automated FUSE-based implementation.
    
    Notes:
    - the final disk image is 401MB (1MB = 2048 x 512B for MBR partition
      table, 400MB for ext4 root partition);
    - fuse-ext2 cannot mount a partition from a file offset, so we create
      a separate partition image file during the build, doubling the disk
      space requirement (400MB for the root partition + 401MB for the whole
      disk image);
    - build now requires `fuse-ext2` instead of `kpartx` to be available on
      the build host;
    
    Closes: #88
    
    Signed-off-by: Alexandru Avadanii <alex@softwareelite.com>

--- snakeware/build.sh
+++ snakeware/build.sh
@@ -1,5 +1,15 @@
+#!/bin/sh
 # This script builds a snakeware image using buildroot.
 # The first and only argument should be the platform (ie x86-64)
+
+cleanup()
+{
+  set +e
+  umount "$MNT" 2>/dev/null
+  rm -f "$RIMG"
+}
+
+trap 'cleanup' INT TERM 0
 
 if [ $# != 1 ]; then
   echo "Invalid number of parameters."
@@ -7,11 +17,21 @@ if [ $# != 1 ]; then
   exit
 fi
 
+set -e
+
+# The final image size will be (`IMG_SIZE` + `BLOCK_SIZE` x `MBR_BLOCK_CNT`)
 IMG_SIZE=400M
 BUILDROOT_VERSION=2020.05
 
 SNAKEWARE=$PWD
 IMG=snakeware.img
+
+RIMG=$IMG.rootpartition
+MNT=$SNAKEWARE/mnt
+
+# Disk/MBR parameter defaults: 2048 sectors x 512B
+BLOCK_SIZE=512
+MBR_BLOCK_CNT=2048
 
 if [ ! -d buildroot ]; then
   git clone -b $BUILDROOT_VERSION https://github.com/buildroot/buildroot.git buildroot --depth 1
@@ -31,8 +51,7 @@ rm -rf $SNAKEWARE/buildroot/overlay
 cp -r $SNAKEWARE/overlay $SNAKEWARE/buildroot/
 
 # run build
-cd buildroot
-make
+make -C buildroot
 
 cd $SNAKEWARE
 
@@ -41,9 +60,19 @@ if [ ! -f buildroot/output/images/rootfs.tar ]; then
   exit
 fi
 
-# create blank image
-rm -f $IMG
-dd if=/dev/zero of=$IMG bs=$IMG_SIZE count=0 seek=1
+# create blank images for the whole disk and the root partition
+rm -f "$IMG" "$RIMG"
+mkdir -p "$MNT"
+dd if=/dev/zero of="$IMG" bs="$BLOCK_SIZE" count="$MBR_BLOCK_CNT" conv=sparse
+
+# NOTE: `fuse-ext2` does not support mounting a partition from a disk file offset,
+# so we create a temporary partition image separately.
+# Since `grub-install` requires a valid partition table to be present when running,
+# we also preallocate the full size of the final disk image instead of just appending
+# the root partition image at the end of the build.
+# This will use twice the disk space during the build process.
+truncate -s "+$IMG_SIZE" "$IMG"
+truncate -s "$IMG_SIZE" "$RIMG"
 
 #  create primary DOS partition, make it bootable, write
 (
@@ -57,10 +86,19 @@ dd if=/dev/zero of=$IMG bs=$IMG_SIZE count=0 seek=1
     echo w
 ) | fdisk $SNAKEWARE/$IMG
 
-# create virtual block device
-sudo kpartx -a $SNAKEWARE/$IMG
+mkfs.ext4 "$RIMG"
+fuse-ext2 "$RIMG" "$MNT" -o force
 
-echo ""
-echo "A virtual block device has been created for $IMG."
-echo "Use lsblk to find the number of this device and run ./img_final.sh <num>"
-echo "Example: ./img_final.sh 0 for loop0"
+# extract generated image to root partition, install grub
+tar -xf $SNAKEWARE/buildroot/output/images/rootfs.tar -C $MNT
+cp $SNAKEWARE/buildroot/output/images/rootfs.cpio $MNT/boot/initrd.img
+mkdir -p $MNT/boot/grub
+grub-install --skip-fs-probe --modules=part_msdos --root-directory="$MNT" "$IMG"
+cp $SNAKEWARE/config/grub.cfg $MNT/boot/grub/grub.cfg
+
+# copy the root partition image into the final disk image
+umount "$MNT"
+dd if="$RIMG" of="$IMG" bs="$BLOCK_SIZE" seek="$MBR_BLOCK_CNT" conv=sparse
+
+chmod a+rwX "$SNAKEWARE/$IMG"
+echo "Succesfully created $IMG."
