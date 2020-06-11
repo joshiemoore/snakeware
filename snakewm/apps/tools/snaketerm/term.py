bce9993709cd431da5e64a9bf4520a7a32d1e02c
callmepavo <z1verdeman2@gmail.com>
2020-06-11 14:43:53 +0800

    Key config now stored in JSON file

--- snakewm/apps/tools/snaketerm/term.py
+++ snakewm/apps/tools/snaketerm/term.py
@@ -9,7 +9,7 @@ import pygame_gui
 from pygame_gui.elements import UITextBox
 
 import os
-
+import json
 
 class SnakeTerm(pygame_gui.elements.UIWindow):
     def __init__(self, pos, manager):
@@ -56,15 +56,25 @@ class SnakeTerm(pygame_gui.elements.UIWindow):
 
         # jump attributes
         self.jump_chars = (" ", "-", "_", "/")
-
+        
+        self.hotkeys = self.get_hotkeys()
+    
+    def get_hotkeys(self):
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        try:
-            with open(current_dir + "/user_hotkeys.txt", "r") as f:
-                self.hotkeys = eval(f.read().replace("\n", ""))
-        except:
-            with open(current_dir + "/default_hotkeys.txt", "r") as f:
-                self.hotkeys = eval(f.read().replace("\n", ""))
-
+        try: # first attmept to load user hotkeys
+            with open(current_dir + "/user_hotkeys.json", "r") as f:
+                raw_json = json.load(f)
+        except: # if that file doesn't exist use default
+            with open(current_dir + "/default_hotkeys.json", "r") as f:
+                raw_json = json.load(f)
+        # run through each value and add "self." then run through eval()
+        key_config = {}
+        for mod_key in raw_json.keys():
+            key_config[mod_key] = {}
+            for normal_key in raw_json[mod_key].keys():
+                key_config[mod_key][normal_key] = eval("self." + raw_json[mod_key][normal_key])
+        return key_config
+        
     def set_text(self, text):
         self.textbox.html_text = text.replace("\n", "<br>")
         self.textbox.rebuild()
