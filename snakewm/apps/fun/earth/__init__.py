b2b64332b3d17d353b5746c1607c4dcfe4e25b85
Martin C. Doege <mdoege@compuserve.com>
2020-06-11 16:08:37 +0200

    add Earth

---
+++ snakewm/apps/fun/earth/__init__.py
@@ -0,0 +1,14 @@
+from .earth import Earth
+
+
+def load(manager, params):
+    """
+    Create and launch a new instance of Earth.
+    """
+    # default position
+    pos = (100, 100)
+
+    if params is not None and len(params) > 0:
+        pos = params[0]
+
+    Earth(pos, manager)
