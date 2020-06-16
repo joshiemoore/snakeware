0d862ebef5140f9dc503d04a86553f84e7546248
Martin C. Doege <mdoege@compuserve.com>
2020-06-16 15:53:00 +0200

    Add cell

---
+++ snakewm/apps/fun/cell/README.md
@@ -0,0 +1,13 @@
+# Cell
+
+Cell runs an elementary cellular automaton (https://en.wikipedia.org/wiki/Elementary_cellular_automaton), selecting the rule, colors, and initial conditions randomly.
+
+The program uses the EGA/Sega Master System color palette (64 colors, 4 levels per channel). The window is resizable and will start a new automaton on each resize event.
+
+# Author
+
+Martin C. Doege
+
++ github: https://github.com/mdoege
+
++ date: 16 Jun 2020
---
+++ snakewm/apps/fun/cell/cell.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+# Cellular automaton
+
+import pygame, random
+import pygame_gui
+from pygame_gui.elements.ui_image import UIImage
+
+RULES = (
+    18,
+    22,
+    26,
+    30,
+    41,
+    45,
+    54,
+    60,
+    90,
+    102,
+    105,
+    106,
+    110,
+    122,
+    126,
+    146,
+    150,
+    154,
+    182,
+)
+RES = 300, 500
+NCELL = RES[0]
+
+
+def getcol():
+    return [85 * random.randint(0, 3) for q in (1, 2, 3)]
+
+
+class Cell(pygame_gui.elements.UIWindow):
+    DIMS = RES
+
+    def __init__(self, pos, manager):
+        super().__init__(
+            pygame.Rect(pos, (self.DIMS[0] + 32, self.DIMS[1] + 60)),
+            manager=manager,
+            window_display_title="cell",
+            object_id="#cell",
+            resizable=True,
+        )
+
+        self.dsurf = UIImage(
+            pygame.Rect((0, 0), self.DIMS),
+            pygame.Surface(self.DIMS).convert(),
+            manager=manager,
+            container=self,
+            parent_element=self,
+        )
+        self.screen = pygame.Surface(self.DIMS)
+        self.manager = manager
+        self.cell = [random.randint(0, 1) for q in range(2 + NCELL)]
+        self.rule = random.choice(RULES)
+        self.gen = 1
+        self.line = 0
+        self.COL = getcol(), getcol()
+
+    def process_event(self, event):
+        super().process_event(event)
+        r = super().get_abs_rect()
+        if event.type == pygame.MOUSEBUTTONUP and (
+            r.w != self.DIMS[0] + 32 or r.h != self.DIMS[1] + 60
+        ):
+            self.DIMS = r.w - 32, r.h - 60
+            super().kill()
+            self.__init__((r.left, r.top), self.manager)
+
+    def newgen(self):
+        self.gen += 1
+        cell2 = [0 for q in range(2 + NCELL)]
+        for x in range(NCELL):
+            s = 2 ** (4 * self.cell[x] + 2 * self.cell[x + 1] + self.cell[x + 2])
+            if s & self.rule:
+                cell2[x + 1] = 1
+        # make cyclic
+        cell2[0] = self.cell[-2]
+        cell2[-1] = self.cell[1]
+        self.cell = cell2
+
+    def update(self, delta):
+        super().update(delta)
+        self.line += 1
+        self.screen.scroll(dy=-1)
+        pygame.draw.line(
+            self.screen,
+            (0, 0, 0),
+            (0, self.DIMS[1] - 1),
+            (self.DIMS[0] - 1, self.DIMS[1] - 1),
+            1,
+        )
+        scrollfac = max(1, int(self.DIMS[0] / RES[0]))
+        if self.line % scrollfac == 0:
+            self.newgen()
+        for x in range(self.DIMS[0]):
+            xp = int(x / self.DIMS[0] * NCELL)
+            pygame.draw.line(
+                self.screen,
+                self.COL[self.cell[xp + 1]],
+                (x, self.DIMS[1] - 1),
+                (x, self.DIMS[1] - 1),
+                1,
+            )
+        super().set_display_title(
+            "cell (rule %u, generation %u)" % (self.rule, self.gen)
+        )
+        self.dsurf.image.blit(self.screen, (0, 0))
