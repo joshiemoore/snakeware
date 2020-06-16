8965ba99e20686f286a430d54f9c90aad9a599f3
callmepavo <z1verdeman2@gmail.com>
2020-06-16 12:16:35 +0800

    Added buttons and counting

--- snakewm/apps/tools/stopwatch/stopwatch.py
+++ snakewm/apps/tools/stopwatch/stopwatch.py
@@ -1,13 +1,13 @@
 import pygame
 import pygame_gui
 
-import datetime
+import time
 
 
 class Stopwatch(pygame_gui.elements.UIWindow):
     def __init__(self, pos, manager):
         super().__init__(
-            pygame.Rect(pos, (300, 300)),
+            pygame.Rect(pos, (335, 110)),
             manager=manager,
             window_display_title="stopwatch",
             object_id="#stopwatch",
@@ -15,42 +15,57 @@ class Stopwatch(pygame_gui.elements.UIWindow):
         )
 
         self.textbox = pygame_gui.elements.UITextBox(
-            "",
-            relative_rect=pygame.Rect(1, 1, 199, 299),
+            "0 : 0 : 0",
+            relative_rect=pygame.Rect(1, 1, 99, 49),
             manager=manager,
             container=self,
-            anchors={
-                "left": "left",
-                "right": "right",
-                "top": "top",
-                "bottom": "bottom",
-            },
         )
-        button_layout_rect = pygame.Rect(0, 0, 100,50)
-        button_layout_rect.topright = (-1,-1)
-        self.button = pygame_gui.elements.UIButton(
+
+        button_layout_rect = pygame.Rect(100, 0, 100, 50)
+        self.start_button = pygame_gui.elements.UIButton(
             relative_rect=button_layout_rect,
-            anchors={"left" : "right",
-                     "right" : "right",
-                     "top" : "top",
-                     "bottom" : "top"},
-            text="s",
+            text="start/pause",
             container=self,
             manager=manager,
+            object_id="#start_button",
         )
+
+        button2_layout_rect = pygame.Rect(200, 0, 100, 50)
+        self.reset_button = pygame_gui.elements.UIButton(
+            relative_rect=button2_layout_rect,
+            text="reset",
+            container=self,
+            manager=manager,
+            object_id="#reset_button",
+        )
+
+        self.time_counter = 0
+        self.currently_counting = False
 
     def process_event(self, event):
         super().process_event(event)
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == self.start_button:
+                    self.currently_counting = not self.currently_counting
+                elif event.ui_element == self.reset_button:
+                    self.reset_time()
 
     def update(self, time_delta):
         super().update(time_delta)
+        if self.currently_counting:
+            self.time_counter += time_delta
+            secs = int(self.time_counter % 60)
+            mins = int( (self.time_counter % (60**2)) // 60 )
+            hours = int(self.time_counter // (60**2))
 
-        dt = datetime.datetime.now()
-        # %X formatted clock, %x formatted date
-        current_time = dt.strftime("%X  %x")
-
-        self.set_text(current_time)
-
+            counter_str = f"{str(hours)} : {str(mins)} : {str(secs)}"
+            self.set_text(counter_str)
+    
     def set_text(self, text):
         self.textbox.html_text = text
         self.textbox.rebuild()
+
+    def reset_time(self):
+        self.time_counter = 0
+        self.currently_counting = False
