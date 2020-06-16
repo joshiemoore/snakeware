9612a7ebc906af400ad101ca50efd5f4eb9579c5
Martin C. Doege <mdoege@compuserve.com>
2020-06-16 18:32:52 +0200

    fix cyclic

--- snakewm/apps/fun/cell/cell.py
+++ snakewm/apps/fun/cell/cell.py
@@ -80,8 +80,8 @@ class Cell(pygame_gui.elements.UIWindow):
             if s & self.rule:
                 cell2[x + 1] = 1
         # make cyclic
-        cell2[0] = self.cell[-2]
-        cell2[-1] = self.cell[1]
+        cell2[0] = cell2[-2]
+        cell2[-1] = cell2[1]
         self.cell = cell2
 
     def update(self, delta):
